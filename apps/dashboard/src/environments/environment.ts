fda789abca0443ece26a00607a60fcc91512e362
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-01-23 23:02:04 -0800

    new

--- apps/dashboard/src/environments/environment.ts
+++ apps/dashboard/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://taskmanagementsystem-production-3115.up.railway.app/",
+  apiUrl: "https://taskmanagementsystem-production-3115.up.railway.app/api",
 };
