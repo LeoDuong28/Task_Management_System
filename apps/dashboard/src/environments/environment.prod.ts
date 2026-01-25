2b546984d38b715e72917341006ae19d433c8440
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-01-25 00:30:12 -0800

    new

--- apps/dashboard/src/environments/environment.prod.ts
+++ apps/dashboard/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Replace this with your deployed API URL (must include /api)
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: "https://task-management-api-spvo.onrender.com",
 };
